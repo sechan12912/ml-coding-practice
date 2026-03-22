7e82a0bec4eb3463043cac6f5fd19eb08167ad85
원세찬 <chxnroze@gmail.com>
2026-03-22 20:27:53 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -99,3 +99,10 @@ print(sample_df.agg(['count','mean', 'std', 'min', 'max']))
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/iris.csv'
 iris = pd.read_csv(file_url)
 
+print(iris.head())
+
+print(iris.groupby('class').std())
+
+print(iris.drop('class', axis=1).agg(['sum','mean', 'std']))
+
+# print(iris.agg(['sum','mean', 'std']))    # class 컬럼으로 인해 에러 발생
