5b097fe3250ecb275a790d6cb4f86f7e83d9892d
원세찬 <chxnroze@gmail.com>
2026-03-22 20:18:52 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -48,3 +48,10 @@ print(netflix.head())
 
 print(netflix['release_year'])
 print(netflix['release_year'] > 2015)
+
+more2015 = netflix[netflix['release_year'] > 2015]
+print(more2015.head(10))
+
+print(~(netflix['release_year'] > 2015))
+less2015 = netflix[~(netflix['release_year'] > 2015)]
+print(less2015.head())
