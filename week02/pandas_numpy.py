3b050a1a459297cac67f248feaa7661107cf123c
원세찬 <chxnroze@gmail.com>
2026-03-22 20:36:53 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -134,3 +134,34 @@ data = {
     'english': [90, 87, 85, 88, 92]
 }
 
+# Dataframe 생성
+df = pd.DataFrame(data)
+print(df.head())
+
+# 개인별 과목 점수의 평균값 계산 (axis=1)
+df['average'] = df[['math', 'science', 'english']].mean(axis=1)
+print(df)
+
+# 이름과 평균값만을 포함하는 새로운 데이터프레임 생성
+average_df = df[['name', 'average']]
+print(average_df)
+
+# 2.2 넘파이
+import numpy as np
+
+print(np.array([1,2,3]))
+
+print(np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]]))
+
+print(np.array([[[1,2,3],
+                [4,5,6],
+                [7,8,9]],
+                [[1,2,3],
+                [4,5,6],
+                [7,8,9]],
+                [[1,2,3],
+                [4,5,6],
+                [7,8,9]]]))
+
