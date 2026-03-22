2bf3e9978f5442f5b75a5772f91517a9040504a6
원세찬 <chxnroze@gmail.com>
2026-03-22 20:37:53 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -176,3 +176,15 @@ print(sample_df.columns)            # 기존 프레임워크에서 컬럼명 가
 
 print(pd.DataFrame(sample_np, columns = sample_df.columns))      # 새 프레임워크에 컬럼명 붙이기
 
+print(sample_np)
+print(sample_np[0])
+print(sample_np[0,2])
+print(sample_np[0:3,2:4])
+print(sample_np[:,2])
+
+np_a = np.array([[1,3], [0,-2]])
+print(np_a)
+print(np_a + 10)
+print(np_a - 5)
+print(np_a * 2)
+print(np_a+10 / 3)
