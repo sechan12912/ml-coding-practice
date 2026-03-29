c63a94d9be26e48c5ddb157a0a2d8cf4aa4f7327
원세찬 <chxnroze@gmail.com>
2026-03-29 20:22:47 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -233,3 +233,29 @@ plt.legend(violin_plot['bodies'], ['1st Class', '2nd Class', '3rd Class'],
            title='Pclass', loc="upper right")
 plt.savefig('Figure10.png')
 plt.close()
+
+## **에러 바 : 요금의 평균과 표준편차 표현하기**
+
+# 각 부모와 자녀의 수에 대한 요금의 평균과 표준 편차 계산
+fare_means = titanic.groupby('Parch')['Fare'].mean()  # 평균
+print(fare_means, '\n')
+
+fare_std = titanic.groupby('Parch')['Fare'].std()     # 표준 편차
+print(fare_std)
+
+"""* 에러바는 데이터의 표준 편차를 나타내는 경우에는 길이가 길수록 해당 그룹의 데이터가 퍼져 있음을 의미"""
+
+# 에러바로 요금의 평균과 표준 편차 표현
+plt.figure(figsize=(10, 6))
+
+# 에러바 생성
+plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='o',
+             capsize=5, capthick=1, label='Fare')
+
+plt.title('Error Bar Plot of Fare by Parch')
+plt.xlabel('Parch')
+plt.ylabel('Fare')
+plt.xticks(fare_means.index)
+plt.legend()
+plt.savefig('Figure11.png')
+plt.close()
