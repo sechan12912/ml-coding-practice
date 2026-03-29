5ccef111f06b25881e0856f33c749ecdf7582707
원세찬 <chxnroze@gmail.com>
2026-03-29 19:02:46 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -32,3 +32,26 @@ plt.xticks([1, 2, 3])
 plt.grid(True)
 plt.savefig('Figure01.png')         # 결과를 그림파일로 저장
 plt.close()                         # 다음 Plot을 새로 그리기 위해 plt 닫기
+
+"""### **수직 막대 그래프 : 각 승선 항구에 따른 생존자 수 확인하기**"""
+
+# 승선 항구에 따른 생존자의 수 계산
+survived_counts = titanic[titanic['Survived'] == 1]['Embarked'].value_counts()
+print(survived_counts)
+
+# 막대 그래프 그리기
+plt.bar(survived_counts.index, survived_counts,
+        color = ['mediumorchid', 'darkviolet', 'indigo'])
+plt.title('Survived Counts by Embarked Port on Titanic')
+plt.xlabel('Embarked Port')
+plt.ylabel('Count')
+plt.xticks(survived_counts.index, ['Southampton', 'Cherbourg', 'Queenstown'])
+plt.legend(['Survived'], loc='upper right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# 생존자 수 표시
+for i, value in enumerate(survived_counts):
+  plt.text(i, value + 1, str(value), ha='center', va='bottom')
+
+plt.savefig('Figure02.png')
+plt.close()
