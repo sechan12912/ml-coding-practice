1744da764521e0bffe2e63d10f07e5dffdc0052b
원세찬 <chxnroze@gmail.com>
2026-03-29 20:44:18 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -386,3 +386,14 @@ ax1.legend(loc='upper right', bbox_to_anchor=(1, 1)) # 범례
 
 # .twinx() 함수를 사용하여 두 번째 그래프와 축을 공유하는 새로운 Axes 객체 생성
 ax2 = ax1.twinx()
+
+# 두 번째 그래프 설정 (막대 그래프)
+ax2.bar(x, y2, color='deeppink', alpha=0.7, width=0.7, label='Survived')
+ax2.set_ylabel('Survived Count', color='deeppink') # 서브플롯 y축에 레이블 추가
+ax2.tick_params(axis='y', labelcolor='deeppink')
+ax2.legend(loc='upper right', bbox_to_anchor=(1, 0.9))
+
+plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+plt.tight_layout()
+plt.savefig('Figure16.png')
+plt.close()
