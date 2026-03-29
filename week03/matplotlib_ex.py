0ecd4ad14f9f18b0b848ef68bc3d8c99b9a6afa5
원세찬 <chxnroze@gmail.com>
2026-03-29 20:02:47 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -154,3 +154,22 @@ plt.yticks(range(len(correlation_matrix.columns)), correlation_matrix.columns)
 plt.title('Correlation Heatmap of Titanic')
 plt.savefig('Figure07.png')
 plt.close()
+
+## **영역 채우기 그래프 : 나이대별 생존자와 사망자 수 표현하기**
+
+# 결측치 처리
+titanic = titanic.dropna(subset=['Age', 'Fare'])
+
+# 나이대별 생존자와 사망자 수 계산하기 위해 범주형 변수로 변환
+age_groups = pd.cut(titanic['Age'], bins=range(0, 81, 5))
+
+# Age, Survived 기준으로 그룹화
+survived_counts = titanic.groupby([age_groups, 'Survived'], observed=False).size().unstack().fillna(0)
+print(survived_counts)
+
+#영역 채우기 그래프 그리기
+plt.figure(figsize=(10, 6))
+
+#나이대별 생존자
+plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
+                 color='purple', alpha=0.9, label='Survived')
