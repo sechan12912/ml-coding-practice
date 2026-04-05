9b280c708ed5f709e1dfec320ce0b3259181280d
원세찬 <chxnroze@gmail.com>
2026-04-05 20:36:12 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -53,3 +53,20 @@ fig = plt.figure(figsize=(15, 5))
 ax1 = fig.add_subplot(1, 2, 1)
 ax2 = fig.add_subplot(1, 2, 2)
 
+# 산점도에 선형 회귀선 표시(fit_reg=True)
+sns.regplot(x='total_bill', y='tip', data=tips, color='blue', scatter_kws={'s': 50, 'alpha': 0.5}, line_kws={'linestyle': '--'}, ax=ax1)
+
+# 산점도에 선형 회귀선 미표시(fit_reg=False)
+sns.regplot(x='total_bill', y='tip', data=tips, color='blue', scatter_kws={'s': 50, 'alpha': 0.5}, line_kws={'linestyle': '--'}, ax=ax2, fit_reg=False)
+
+fig.suptitle('Scatter Plots with Regression Lines', fontsize=16)
+ax1.set_title('fit_reg = True')
+ax2.set_title('fit_reg = False')
+plt.savefig('./week04/Seaborn_Figure03.jpg')
+
+# **히스토그램과 커널 밀도 추정 그래프**
+# 히스토그램과 커널 밀도 추정 그래프 함께 그리기
+sns.histplot(tips['tip'], bins=30, kde=True, color='skyblue')
+
+plt.title('Histogram with KDE for Tips')
+plt.savefig('./week04/Seaborn_Figure04.jpg')
