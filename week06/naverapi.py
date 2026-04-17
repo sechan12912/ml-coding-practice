c2e30e40f088c9d6b406938d4225ddd4dd693479
원세찬 <chxnroze@gmail.com>
2026-04-17 23:35:23 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -49,4 +49,19 @@ def getNaverSearch(node, srcText, page_start, display):
     else:
         return json.loads(responseDecode)
     
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
+
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
     
