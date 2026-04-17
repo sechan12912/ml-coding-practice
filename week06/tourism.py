1181f1946c3acd1c51256aeb89a112fe1696397b
원세찬 <chxnroze@gmail.com>
2026-04-17 23:50:24 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -24,3 +24,8 @@ def main():
   columns = ["입국자국가", "국가코드", "입국연월", "입국자 수"]
   result_df = pd.DataFrame(result, columns = columns)
   result_df.to_csv('./%s_%s_%d_%s.csv' % (natName, ed_cd, nStartYear, dataEND), index = False, encoding = 'cp949')
+
+"""### [CODE 3]"""
+
+def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
+  
