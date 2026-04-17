bf8f32f7238184351855e6b39f06c080fc1b2da4
원세찬 <chxnroze@gmail.com>
2026-04-17 23:26:23 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -10,3 +10,11 @@ def main():
 
     node = 'news'                                       #크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
+    total = jsonResponse['total']
+
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
