0a496404fc43b15d984251c74d2db401ae003de4
원세찬 <chxnroze@gmail.com>
2026-04-17 23:28:23 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -21,4 +21,6 @@ def main():
         for post in jsonResponse['items']:
             cnt += 1
             getPostData(post, jsonResult, cnt)                # [CODE 3]
-            
+
+        start = jsonResponse['start'] + jsonResponse['display']
+        
