343fbd6b0d7c0b57323b2c5203a903019b9738de
원세찬 <chxnroze@gmail.com>
2026-05-05 20:24:45 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
 from sklearn.datasets import fetch_openml
 
-mnist
+mnist = fetch_openml('mnist_784' , as_frame=False)
+
+print(mnist.keys()) # data와 target만 사용
+
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape)      # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y.shape)
+
+import matplotlib.pyplot as plt
+
+def plot_digit(image_data):
+    image = image_data.reshape(28, 28)
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+
+some_digit = X[0]
+plot_digit(some_digit)
+plt.show()
