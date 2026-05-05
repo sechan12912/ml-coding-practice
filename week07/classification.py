f248bb6bf3acc418f1e1daaf8134013c754fba03
원세찬 <chxnroze@gmail.com>
2026-05-05 21:05:46 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -83,4 +83,25 @@ from sklearn.metrics import f1_score
 
 print(f1_score(y_train_5, y_train_pred))
 
+# ROC 곡선
+from sklearn.metrics import roc_auc_score
 
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
+                             method="decision_function")
+roc_auc_score(y_train_5, y_scores)
+
+#다음은 실행하는데 몇 분이 걸릴 수 있음
+from sklearn.ensemble import RandomForestClassifier
+
+forest_clf = RandomForestClassifier(random_state=42)
+
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
+                                    method="predict_proba")
+
+y_scores_forest = y_probas_forest[:, 1]
+y_train_pred_forest = y_probas_forest[:, 1] >= 0.5  # 양성 확률 ≥ 50%
+
+print(f1_score(y_train_5, y_train_pred_forest))
+print(roc_auc_score(y_train_5, y_scores_forest))
+print(precision_score(y_train_5, y_train_pred_forest))
+print(recall_score(y_train_5, y_train_pred_forest))
