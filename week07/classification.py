38f6194d875b77baf462644c0da6dd27a0ee6856
원세찬 <chxnroze@gmail.com>
2026-05-05 21:00:16 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -66,4 +66,5 @@ y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 
 from sklearn.metrics import confusion_matrix
 
-cm = confusion
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
