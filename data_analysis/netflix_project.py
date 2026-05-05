857141a05b2cbb8387ed4053fe0c4ee835d5e771
원세찬 <chxnroze@gmail.com>
2026-05-05 23:32:16 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -173,3 +173,20 @@ netflix_age_country_unstack = netflix_age_country_unstack.loc[age_order, country
 netflix_age_country_unstack = netflix_age_country_unstack.fillna(0)
 netflix_age_country_unstack
 
+`# 나이 그룹에 따른 국가별 넷플릭스 콘텐츠 비율 구하기
+netflix_age_country_unstack = netflix_age_country_unstack.div(netflix_age_country_unstack.sum(axis=0), axis=1)
+netflix_age_country_unstack
+
+plt.figure(figsize=(15, 5))
+
+# 사용자 정의 컬러맵 만들기
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710','#f5f5f1'])
+
+sns.heatmap(netflix_age_country_unstack, cmap = cmap, linewidth=2.5, annot=True, fmt='.0%')
+
+plt.suptitle('Target ages proportion of total content by country',
+             fontweight='bold', fontfamily='serif', fontsize=15)
+plt.title('Here we see interesting differences between countries. Most shows in South Korea are targeted to adults, for instance.',
+          fontsize=12, fontfamily='serif')
+plt.show()
+
