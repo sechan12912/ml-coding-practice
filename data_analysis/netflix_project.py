dc1612ab8f0094910b9fc341c3e1ce217ed38c11
원세찬 <chxnroze@gmail.com>
2026-05-05 21:56:44 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -93,3 +93,14 @@ sns.palplot(['#221f1f', '#b20710', '#e50914', '#f5f5f1'])
 plt.title('Netflix brand palette', loc='left', fontfamily='serif', fontsize=15, y=1.2)
 plt.show()
 
+netflix['title'].str.contains('squid game', na=False, case=False)
+
+# 오징어 게임을 검색한 조건을 넷플릭스 데이터에 넣어서 True인 값만 출력
+netflix[netflix['title'].str.contains('Squid Game', na=False, case=False)]
+
+type_counts = netflix['type'].value_counts()
+print(type_counts)
+
+# 5 x 5 크기의 플롯 만들기
+plt.figure(figsize=(5, 5))
+
