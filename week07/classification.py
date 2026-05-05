eef051dca4740d134f083efa4521d2167a0b82b1
원세찬 <chxnroze@gmail.com>
2026-05-05 20:49:46 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -55,4 +55,6 @@ from sklearn.dummy import DummyClassifier
 
 dummy_clf = DummyClassifier()
 dummy_clf.fit(X_train, y_train_5)
-print(any(dummy_clf, X_train, y_train_5, cv=3,))
+print(any(dummy_clf.predict(X_train)))
+
+print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
