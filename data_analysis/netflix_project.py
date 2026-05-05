6432bcb5e85af5ffee3c70500b3ecb7fcc1900a9
원세찬 <chxnroze@gmail.com>
2026-05-05 22:17:44 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -111,3 +111,17 @@ plt.suptitle('Movie & TV Show distribution', fontfamily='serif', fontsize=15, fo
 plt.title('We see more movies than TV shows on Netflix.', fontfamily='serif', fontsize=12)
 plt.show()
 
+netflix.head(3)
+
+# 넷플릭스 데이터셋의 장르별 등장 횟수 계산
+genres = netflix['listed_in'].str.split(', ', expand=True).stack().value_counts()
+genres
+
+# [1단계] listed_in 열에 있는 장르를 쉼표로 분할하기
+# 예시) 인덱스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
+netflix['listed_in'].str.split(', ', expand=True)
+
+# [2단계] .stack( )을 사용하면 여러 열로 구성한 데이터프레임을 1개의 열로 만들어 쌓음
+# 예시) 인덱스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
+netflix['listed_in'].str.split(', ', expand=True).stack()
+
