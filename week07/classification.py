9e066fb475aa78fec17f619147addfeb82742b30
원세찬 <chxnroze@gmail.com>
2026-05-05 21:05:16 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -77,4 +77,10 @@ from sklearn.metrics import precision_score, recall_score
 
 print(precision_score(y_train_5, y_train_pred))  # == 3530 / (687 + 3530)
 
-print()
+print(recall_score(y_train_5, y_train_pred))  # == 3530 / (1891 + 3530)
+
+from sklearn.metrics import f1_score
+
+print(f1_score(y_train_5, y_train_pred))
+
+
