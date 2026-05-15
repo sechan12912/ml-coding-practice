d01418473f2351810049759c0fa826387e0a1be0
원세찬 <chxnroze@gmail.com>
2026-05-15 23:40:09 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -116,3 +116,25 @@ print(sgd_reg.predict([[1.5]]))
 ridge_reg = Ridge(alpha=0.1, solver="sag", random_state=42)
 ridge_reg.fit(X, y)
 print(ridge_reg.predict([[1.5]]))
+
+# 라쏘 회귀
+
+from sklearn.linear_model import Lasso
+
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+print(lasso_reg.predict([[1.5]]))
+
+# 엘라스틱넷
+
+from sklearn.linear_model import ElasticNet
+
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+print(elastic_net.predict([[1.5]]))
+
+# 로지스틱 회귀
+from sklearn.datasets import load_iris
+
+iris = load_iris(as_frame=True)
+list(iris)
