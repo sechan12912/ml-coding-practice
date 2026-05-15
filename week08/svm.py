e932e584655e2617e507ca67e2184afaffe2ded3
원세찬 <chxnroze@gmail.com>
2026-05-15 23:51:25 +0900

    Updated Changes

--- week08/svm.py
+++ week08/svm.py
@@ -16,3 +16,20 @@ svm_clf = make_pipeline(StandardScaler(),
 svm_clf.fit(X, y)
 
 X_new = [[5.5, 1.7], [5.0, 1.5]]
+
+print(svm_clf.predict(X_new))
+print(svm_clf.decision_function(X_new))
+
+# 비선형 SVM 분류
+
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import PolynomialFeatures
+
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+polynomial_svm_clf = make_pipeline(
+    PolynomialFeatures(degree=3),
+    StandardScaler(),
+    LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
+)
+polynomial_svm_clf.fit(X, y)
