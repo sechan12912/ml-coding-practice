a48841ecf7b2dd4d180f303497a3dba1f08b1a97
원세찬 <chxnroze@gmail.com>
2026-05-15 23:47:55 +0900

    Updated Changes

---
+++ week08/svm.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 선형 SVM 분류
+
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+
