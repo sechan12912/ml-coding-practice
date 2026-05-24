bd7206a2abb7fa4babac9478f607a131a1ba97f3
원세찬 <chxnroze@gmail.com>
2026-05-24 20:26:34 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -45,4 +45,23 @@ X_moons_test, y_moons_test = make_moons(n_samples=1000, noise=0.2,
 print(tree_clf1.score(X_moons_test, y_moons_test))
 print(tree_clf2.score(X_moons_test, y_moons_test))
 
-   
+# 회귀
+
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+
+np.random.seed(42)
+X_quad = np.random.rand(200, 1) - 0.5  # 간단한 랜덤한 입력 특성
+y_quad = X_quad ** 2 + 0.025 * np.random.randn(200, 1)
+
+tree_reg = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg.fit(X_quad, y_quad)
+
+export_graphviz(
+    tree_reg,
+    out_file="regression_tree.dot",
+    feature_names=["x1"],
+    rounded=True,
+    filled=True
+)
+Source.from_file("regression_tree.dot")
