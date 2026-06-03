76c64e7e66b86d8d6d36d463bec7853abbd950f1
원세찬 <chxnroze@gmail.com>
2026-06-03 18:49:22 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -25,3 +25,9 @@ k = 5
 kmeans = KMeans(n_clusters=k, n_init=10, random_state=42)
 y_pred = kmeans.fit_predict(X)
 
+def plot_clusters(X, y=None):
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=1)
+    plt.xlabel("$x_1$")
+    plt.ylabel("$x_2$", rotation=0)
+
+    
