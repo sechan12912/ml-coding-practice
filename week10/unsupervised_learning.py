95ddc302b7313d546e1b09d31123ce0fd6e8fd6c
원세찬 <chxnroze@gmail.com>
2026-06-03 18:54:52 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -62,3 +62,16 @@ plt.axis([1, 8.5, 0, 3500])
 plt.grid()
 plt.show()
 
+from sklearn.metrics import silhouette_score
+
+print(silhouette_score(X, kmeans.labels_))
+
+silhouette_scores = [silhouette_score(X, model.labels_) for model in kmeans_per_k[1:]]
+
+plt.figure(figsize=(8, 3))
+plt.plot(range(2, 10), silhouette_scores, "bo-")
+plt.xlabel("$k$")
+plt.ylabel("실루엣 점수")
+plt.axis([1.8, 8.5, 0.55, 0.8])
+plt.grid()
+plt.show()
