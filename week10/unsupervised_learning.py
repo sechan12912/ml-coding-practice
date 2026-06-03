6004979687c57eecbc25388da1e4056b1bc2992d
원세찬 <chxnroze@gmail.com>
2026-06-03 18:53:52 +0900

    Updated Changes

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -49,3 +49,16 @@ print(kmeans.inertia_)
 
 print(kmeans.score(X))
 
+kmeans_per_k = [KMeans(n_clusters=k, n_init=10, random_state=42).fit(X) for k in range(1, 10)]
+inertias = [model.inertia_ for model in kmeans_per_k]
+
+plt.figure(figsize=(8, 3.5))
+plt.plot(range(1, 10), inertias, "bo-")
+plt.xlabel("$k$")
+plt.ylabel("이너셔")
+plt.annotate("", xy=(5, inertias[4]), xytext=(4.45, 650), arrowprops=dict(facecolor='black', shrink=0.1))
+plt.text(4.5, 650, "엘보", horizontalalignment="center")
+plt.axis([1, 8.5, 0, 3500])
+plt.grid()
+plt.show()
+
